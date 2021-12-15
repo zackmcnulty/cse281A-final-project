a38367ea6ab22721f1b39ece9e525d1fdf878a19
Zachary McNulty <zjmcnulty@gmail.com>
2021-12-15 15:12:12 -0800

    finished PCA

--- pca.py
+++ pca.py
@@ -8,7 +8,7 @@ from scipy.sparse import csr_matrix, load_npz
 import allensdk
 from allensdk.brain_observatory.behavior.behavior_project_cache import VisualBehaviorOphysProjectCache
 
-from sklearn.decomposition import TruncatedSVD # designed to work efficiently on sparse matrices
+from sklearn.decomposition import TruncatedSVD
 
 pd.set_option('display.max_columns', None)
 
@@ -26,6 +26,13 @@ TruncatedPCA
 
 if __name__ == "__main__":
 
+    # filepath to the sparse images matrix (produced by image_processing.py) and the relevant OPHYS experiemnt ids
+    sparse_filepath = Path("data\\results\\sparse_images.npz")
+    processed_ids_filepath = Path("data\\results\\processed_image_ids.csv")
+
+    # filepath to save the PCA coordinates to
+    pca_filepath = Path("data\\results\\pca\\pca_coordinates.csv")
+
     # Confirming your allensdk version
     print(f"Your allensdk version is: {allensdk.__version__}")
 
@@ -37,28 +44,12 @@ if __name__ == "__main__":
     # High-level overview of the behavior sessions in Visual Behavior Dataset (Pandas Dataframe)
     all_ophys_sessions = cache.get_ophys_session_table().sort_index()
 
-    sparse_filepath = Path("data\\results\\sparse_images.npz")
-    processed_ids_filepath = Path("data\\results\\processed_image_ids.csv")
 
     # images are stored as rows of the following sparse matrix
     sparse_images = load_npz(sparse_filepath)
     processed_ids = np.genfromtxt(processed_ids_filepath)
 
     num_images = len(processed_ids)
-
-    # plot one sparse image to check sparse_images were stored correctly
-    # index = 10
-    # plt.subplot(121)
-    # plt.imshow(sparse_images[index, :].toarray().reshape((512, 512)), cmap='gray')
-    #
-    # plt.subplot(122)
-    # test_experiment = cache.get_behavior_ophys_experiment(ophys_experiment_id=processed_ids[index])
-    # plt.imshow(test_experiment.max_projection.data, cmap='gray')
-    # plt.show()
-
-    # for i in range(num_images):
-    #     plt.imshow(sparse_images[i, :].toarray().reshape(512, 512), cmap='gray')
-    #     plt.show()
 
     n_components = 750 # number of SVD components to calculate
 
@@ -67,21 +58,19 @@ if __name__ == "__main__":
 
     print(np.cumsum(svd_model.explained_variance_ratio_))
 
+    # Show what some of the principal components look like
     # for i in range(n_components):
     #     plt.imshow(svd_model.components_[i, :].reshape(512, 512), cmap='gray')
     #     plt.show()
 
 
-    # images transformed into SVD basis
+    # Show images reconstructed from the their coordinates in the SVD (principal components) basis
     svd_images = svd_model.transform(sparse_images)
-    for i in range(num_images):
-        svd_coordinates = svd_images[i, :]
+    # for i in range(num_images):
+    #     svd_coordinates = svd_images[i, :]
+    #
+    #     low_dim_image = svd_coordinates @ svd_model.components_
+    #     plt.imshow(low_dim_image.reshape((512, 512)), cmap='gray')
+    #     plt.show()
 
-        low_dim_image = svd_coordinates @ svd_model.components_
-        plt.imshow(low_dim_image.reshape((512, 512)), cmap='gray')
-        plt.show()
-
-
-
-
-
+    np.savetxt(pca_filepath, svd_images, delimiter=",")
