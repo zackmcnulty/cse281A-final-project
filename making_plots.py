21b6dcdf5602ce5c84e2b049f2c8468a1e45d6b8
Zachary McNulty <zjmcnulty@gmail.com>
2021-12-15 23:08:18 -0800

    added pca plots to making_plots.py

--- making_plots.py
+++ making_plots.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 import pandas as pd
 from pathlib import Path
 import seaborn as sn
+import numpy as np
+import time
 
-sn.set_theme()
-sn.set_context("talk")
+import allensdk
+from allensdk.brain_observatory.behavior.behavior_project_cache import VisualBehaviorOphysProjectCache
+
+
 
 
 """
@@ -13,7 +18,94 @@ the other files.
 
 """
 
-# HBayes Plots ###########################
+
+# PCA Plots #######################################################
+pca_filepath = Path("data\\results\\pca\\pca_coordinates.csv")
+processed_ids_filepath = Path("data\\results\\processed_image_ids.csv")
+
+fontsize=17
+
+processed_ids = np.genfromtxt(processed_ids_filepath,
+                              dtype=int)  # gives experiment_ids for each image in pca_coordinates
+pca_coordinates = np.genfromtxt(pca_filepath, delimiter=",")  # each row is pca coordinates of 1 image
+
+data_storage_directory = Path("data")
+
+cache = VisualBehaviorOphysProjectCache.from_s3_cache(cache_dir=data_storage_directory)
+all_ophys_experiments = cache.get_ophys_experiment_table()
+all_ophys_experiments = all_ophys_experiments.loc[processed_ids]
+
+# +1 = familiar, 0 = novel
+y = all_ophys_experiments['experience_level'].values
+
+pca_coordinates = np.genfromtxt(Path("data\\results\\pca\\pca_coordinates.csv"), delimiter=",")
+familiar_coordinates = pca_coordinates[y == "Familiar", :]
+novel_coordinates = pca_coordinates[y != "Familiar", :]
+
+
+plt.figure(2)
+
+ax = plt.axes(projection="3d")
+ax.scatter3D(familiar_coordinates[:, 0], familiar_coordinates[:, 1], familiar_coordinates[:, 2],
+             label="familiar",
+             color="orange",
+             alpha=0.5)
+ax.scatter3D(novel_coordinates[:, 0], novel_coordinates[:, 1], novel_coordinates[:, 2],
+             label="novel",
+             color="blue",
+             alpha=0.5)
+plt.legend(fontsize=fontsize)
+plt.title("Distribution Components 1-3", fontsize=fontsize, fontweight='bold')
+ax.set_xlabel('1st Component', fontweight='bold', fontsize=fontsize)
+ax.set_ylabel('2nd Component', fontweight='bold', fontsize=fontsize)
+ax.set_zlabel('3rd Component', fontweight='bold', fontsize=fontsize)
+
+
+plt.show()
+
+plt.figure(3)
+
+ax = plt.axes(projection="3d")
+ax.scatter3D(familiar_coordinates[:, 3], familiar_coordinates[:, 4], familiar_coordinates[:, 5],
+             label="familiar",
+             color="orange",
+             alpha=0.5)
+ax.scatter3D(novel_coordinates[:, 3], novel_coordinates[:, 4], novel_coordinates[:, 5],
+             label="novel",
+             color="blue",
+             alpha=0.5)
+plt.legend(fontsize=fontsize)
+plt.title("Distribution Components 4-6", fontsize=fontsize, fontweight='bold')
+ax.set_xlabel('4th Component', fontweight ='bold', fontsize=fontsize)
+ax.set_ylabel('5th Component', fontweight ='bold', fontsize=fontsize)
+ax.set_zlabel('6th Component', fontweight ='bold', fontsize=fontsize)
+
+plt.show()
+
+plt.figure(4)
+
+ax = plt.axes(projection="3d")
+ax.scatter3D(familiar_coordinates[:, 6], familiar_coordinates[:, 7], familiar_coordinates[:, 8],
+             label="familiar",
+             color="orange",
+             alpha=0.5)
+ax.scatter3D(novel_coordinates[:, 6], novel_coordinates[:, 7], novel_coordinates[:, 8],
+             label="novel",
+             color="blue",
+             alpha=0.5)
+plt.legend(fontsize=fontsize)
+plt.title("Distribution Components 7-9", fontsize=fontsize, fontweight='bold')
+ax.set_xlabel('7th Component', fontweight ='bold', fontsize=fontsize)
+ax.set_ylabel('8th Component', fontweight ='bold',fontsize=fontsize)
+ax.set_zlabel('9th Component', fontweight ='bold', fontsize=fontsize)
+
+plt.show()
+
+
+# HBayes Plots ####################################
+
+sn.set_theme()
+sn.set_context("talk")
 
 filepath = "data\\results\\hbayes\\"
 
@@ -28,7 +120,7 @@ novel_table.index.name = "mouse_id"
 
 num_bins = 13  # number histogram bins
 
-plt.figure()
+plt.figure(1)
 plt.subplot(131)
 plt.hist(familiar_table['bayes_TPR'].to_numpy(), density=True, bins=num_bins, alpha=0.5, label="familiar", color='blue')
 plt.hist(novel_table['bayes_TPR'].to_numpy(), density=True, bins=num_bins, alpha=0.5, label="novel", color='orange')
